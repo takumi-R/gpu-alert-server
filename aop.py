092562183f07d9c16f5203a8cd818cdff8114362
takumi sato <193427015@ccmailg.meijo-u.ac.jp>
2020-05-15 09:36:27 +0900

    add database

--- aop.py
+++ aop.py
@@ -2,17 +2,14 @@ from flask import Flask,request,abort,jsonify
 import numpy as np
 import os
 from slackclient import SlackClient
+import database as D
 app = Flask(__name__)
 verfica_token = os.environ.get("VERFICA_TOKEN")
 slack_token = os.environ.get("SLACK_API_TOKEN")
-gpu_dict = dict()
-gpu_dict_users =[] 
-
-def add_alarm(channel_id,gpu_id = None,gpu_mem = 1000):
-    gpu_dict_users.append({ "channel_id":channel_id,'gpu_id':gpu_id,'mem' : np.array(gpu_mem) })
-def send_alarm(channel_id,gpu_id = None):
+gpu_database = D.database_set()
+def send_alarm(channel_id,pc_id):
         print("sending")
-        ls_str = "gpu_id:" + str(gpu_id) + " has enough memory for you"
+        ls_str = "pc_id:" + str(pc_id) + " has enough memory for you"
         sc = SlackClient(slack_token)
         M=sc.api_call(
             "chat.postMessage",
@@ -22,6 +19,7 @@ def send_alarm(channel_id,gpu_id = None):
 
 @app.route('/gpu_status',methods=["POST"])
 def hello():
+    global gpu_dict_users   
     if not request.json :
           abort(400)
     content = request.json
@@ -32,11 +30,10 @@ def hello():
           abort(400)
     if not 'gpu_mem' in content:
           abort(400)
-    gpu_dict[content['gpu_id']] = { 'size' :content['gpu_size'] , 'mem' : content['gpu_mem'],'name' : content['name'] }
-    for gpu_dict_int in range(len(gpu_dict_users)):
-        if int((np.array(gpu_dict[gpu_dict_users[gpu_dict_int]['gpu_id']]['size']) - gpu_dict_users[gpu_dict_int]['mem']).max())<0:
-            send_alarm(gpu_dict_users[gpu_dict_int]['channel_id'],gpu_dict_users[gpu_dict_int]['gpu_id'])
-            del gpu_dict_users[gpu_dict_int]
+    gpu_database.set_gpu(content)      
+    channel_id_list =  gpu_database.check_reserv()   
+    for channel_id in channel_id_list:
+        send_alarm(channel_id[0],channel_id[1])
     return "200"
 @app.route('/slack_gpu',methods=["POST"])
 def slack_gpu():
@@ -46,11 +43,8 @@ def slack_gpu():
         abort(400)
     if request.form['text'] == str("ls") :
         ls_str = ""
-        for gpu_dict_key in gpu_dict.keys():
-            ls_str += "gpu_id:" + str(gpu_dict_key) + "    hostname:" + str(gpu_dict[gpu_dict_key]['name'])
-        return jsonify({'text':ls_str})
-    if request.form['text'] == str("help") :
-        ls_str="         User  Commands\nname\n      /remind_gpu -- set alert for gpu memory\nSYNOPSIS\n      /remind_gpu [OPTIONS]\n      /remind_gpu [gpu_id] [MEMORY_LIST]\nDESCRIPTION\n    Alerts user when gpu_ids memory usage get lower than MEMORY_LIST\n    ls\n        prints the correspondence between gpu_id:hostname\n    help\n        prints this message\nEXAMPLE\n    /remind_gpu 0 500,500\n        When gpu_id=0s first gpu memory usage gets lower than 500MiB and\n        second gpu memory usage gets lower than 500MiB the alert will be sent"
+        for ls_name_item in gpu_database.ls_name():
+             ls_str += "pc_id:" + str(ls_name_item[0]) + "    hostname:" + str(ls_name_item[1])
         return jsonify({'text':ls_str})
     elif len(request.form['text'].split(" ")) == 2:
         gpu_id,gpu_mem= request.form['text'].split(" ")
@@ -58,12 +52,12 @@ def slack_gpu():
             gpu_id = int(gpu_id)
             gpu_mem = map(int,gpu_mem.split(","))
             gpu_mem = list(gpu_mem)
+            gpu_database.set_reserv(request.form['channel_id'],gpu_id,gpu_mem)      
         except:
-            return jsonify({'text':relay_str + "unknown format"})
-        add_alarm(request.form['channel_id'],gpu_id,gpu_mem)
+            return jsonify({'text':relay_str + "failed to set alert"})
         return jsonify({'text':relay_str+"alert has been set"})
     else:
-        ls_str="         User  Commands\nname\n      /remind_gpu -- set alert for gpu memory\nSYNOPSIS\n      /remind_gpu [OPTIONS]\n      /remind_gpu [gpu_id] [MEMORY_LIST]\nDESCRIPTION\n    Alerts user when gpu_ids memory usage get lower than MEMORY_LIST\n    ls\n        prints the correspondence between gpu_id:hostname\n    help\n        prints this message\nEXAMPLE\n    /remind_gpu 0 500,500\n        When gpu_id=0s first gpu memory usage gets lower than 500MiB and\n        second gpu memory usage gets lower than 500MiB the alert will be sent"
+        ls_str="         User  Commands\nname\n      /remind_gpu -- set alert for gpu memory\nSYNOPSIS\n      /remind_gpu [OPTIONS]\n      /remind_gpu [pc_id] [MEMORY_LIST]\nDESCRIPTION\n    Alerts user when pc_ids memory usage get lower than MEMORY_LIST\n    ls\n        prints the correspondence between pc_id:hostname\n    help\n        prints this message\nEXAMPLE\n    /remind_gpu 0 500,500\n        When pc_id=0s first gpu memory usage gets lower than 500MiB and\n        second gpu memory usage gets lower than 500MiB the alert will be sent"
         return jsonify({'text':ls_str})
 if __name__ == '__main__':
         app.run(host='0.0.0.0',ssl_context='adhoc',port=8081)
---
+++ database.py
@@ -0,0 +1,192 @@
+import mysql.connector
+import os
+USERNAME_SQL = os.environ.get("USERNAME_SQL")
+PASSWD_SQL = os.environ.get("PASSWD_SQL")
+"""
+ql> create table pcname( pcid int NOT NULL AUTO_INCREMENT, name text,PRIMARY KEY (pcid));
+
+create table pcgpu( gpuid int NOT NULL AUTO_INCREMENT, pcid int NOT NULL, pcgpuid int NOT NULL,memory int NOT NULL,PRIMARY KEY (gpuid),FOREIGN KEY (pcid) REFERENCES pcname(pcid) );
+mysql> create table reserv( reservid int NOT NULL AUTO_INCREMENT, channelid text  NOT NULL,pcid int NOT NULL ,PRIMARY KEY (reservid),FOREIGN KEY (pcid) REFERENCES pcname(pcid) );
+create table reservgpu(reservgpuid int NOT NULL AUTO_INCREMENT,reservid int NOT NULL,gpuid int NOT NULL,memory text NOT NULL ,PRIMARY KEY (reservgpuid),FOREIGN KEY (reservid) REFERENCES reserv(reservid),FOREIGN KEY (gpuid) REFERENCES pcgpu(gpuid) );
+
+mysql> create table reservgpu(reservgpuid int NOT NULL AUTO_INCREMENT,reservid int NOT NULL,memory text NOT NULL ,PRIMARY KEY (reservgpuid),FOREIGN KEY (reservid) REFERENCES reserv(reservid) );
+
+
+
+
+"""
+class database_set:
+    def __init__(self):
+       self.config = {
+                'user': USERNAME_SQL,
+                'password': PASSWD_SQL,
+                'host': 'localhost',
+                'database' : 'Aquadatabase'
+                 }        
+
+       print(self.config)
+       try:
+           print(self.config)
+           self.cnx = mysql.connector.connect(**self.config)
+           self.cursor = self.cnx.cursor()
+
+       except mysql.connector.Error as err:
+           if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+               print('Cannnot connect database.')
+           else:
+               print(err)
+
+
+
+    def set_gpu(self,content):
+        add_pc = ("INSERT INTO pcname(name) "
+                  "SELECT %s"
+                  "where NOT EXISTS (select name from pcname where name=%s)")
+        get_pcid = ("SELECT pcid FROM pcname "
+                    "where  name = %s")
+
+        get_gpuid = ("SELECT gpuid FROM pcgpu "
+                    "where pcid=%s and pcgpuid=%s ")
+
+        check_pc = ("select pcid,pcgpuid,memory from pcgpu WHERE pcid=%s and pcgpuid=%s")
+
+
+        add_gpu = ("INSERT INTO pcgpu(pcid,pcgpuid,memory) "
+                   "VALUES (%s,%s,%s)")
+        update_gpu = ("UPDATE pcgpu "
+                      "SET pcid=%s,pcgpuid=%s,memory=%s "
+                      "WHERE gpuid=%s")
+
+        self.cursor.execute("SELECT * FROM pcname")
+        print(self.cursor.fetchall())
+
+        self.cursor.execute(add_pc,(str(content['name']),str(content['name'])))
+        self.cursor.execute("SELECT * FROM pcname")
+        print(self.cursor.fetchall())
+
+        self.cursor.execute(get_pcid,(str(content['name']),))
+        pcid = self.cursor.fetchall()
+        print(pcid)
+
+        for gpu_size_i in range(len(content['gpu_size'])):
+            self.cursor.execute(check_pc,(pcid[0][0],gpu_size_i))
+            if not len(self.cursor.fetchall()):
+                self.cursor.execute(add_gpu,(pcid[0][0],gpu_size_i,str(content['gpu_size'][gpu_size_i])))
+            else:    
+                self.cursor.execute(get_gpuid,(pcid[0][0],gpu_size_i))
+                gpuid = self.cursor.fetchall()
+                self.cursor.execute(update_gpu,(pcid[0][0],gpu_size_i,str(content['gpu_size'][gpu_size_i]),gpuid[0][0]))
+
+
+
+        self.cnx.commit()
+    def set_reserv(self,channel_id,pc_id,gpu_mem):
+        #create table reservgpu(reservgpuid int NOT NULL AUTO_INCREMENT,reservid int NOT NULL,gpuid int NOT NULL,memory text NOT NULL ,PRIMARY KEY (reservgpuid),FOREIGN KEY (reservid) REFERENCES reserv(reservid),FOREIGN KEY (gpuid) REFERENCES pcgpu(gpuid) );
+
+        add_reserv = ("INSERT INTO reserv(channelid ,pcid) "
+                      "VALUES (%s,%s)")
+
+        get_reserv = ("SELECT reservid FROM reserv "
+                      "where channelid=%s and pcid=%s ")
+
+        check_reserv = ("select channelid,pcid from reserv WHERE channelid=%s and pcid=%s")
+        
+
+        check_reservgpu = ("select reservid,memory from reservgpu WHERE reservid=%s and gpuid=%s")
+
+        add_reservgpu = ("INSERT INTO reservgpu(reservid,memory,gpuid) "
+                             "VALUES (%s,%s,%s)")
+
+        update_reservgpu = ("UPDATE reservgpu "
+                            "SET reservid=%s,memory=%s,gpuid=%s "
+                            "WHERE reservgpuid=%s")
+
+        get_reservgpuid = ("SELECT reservgpuid from reservgpu "
+                      "where reservid=%s ,memory=%s ,gpuid=%s")
+
+        get_gpuid  = ("SELECT gpuid from pcgpu "
+                      "where pcid=%s and pcgpuid=%s ")
+
+        self.cursor.execute(check_reserv,(channel_id,pc_id))
+        if not len(self.cursor.fetchall()):
+            self.cursor.execute(add_reserv,(channel_id,pc_id))
+        self.cursor.execute(get_reserv,(channel_id,pc_id))
+        reservid = self.cursor.fetchall()
+
+        for gpu_size_i in range(len(gpu_mem)):
+
+            self.cursor.execute(get_gpuid,(pc_id,gpu_size_i))
+            gpuid = self.cursor.fetchall()
+            print(gpuid)
+            self.cursor.execute(check_reservgpu,(reservid[0][0],gpu_size_i))
+            if not len(self.cursor.fetchall()):
+                self.cursor.execute(add_reservgpu,(reservid[0][0],gpu_mem[gpu_size_i],gpuid[0][0]))
+            else:    
+                self.cursor.execute(update_reservgpu,(reservid[0][0],gpu_mem[gpu_size_i],gpuid[0][0]))
+
+        
+        self.cnx.commit()
+    def check_reserv(self):
+        get_reserv_single = ("SELECT DISTINCT reservid  FROM pcgpu "
+                            "INNER JOIN reservgpu ON pcgpu.gpuid = reservgpu.gpuid "
+                            "WHERE pcgpu.memory <= reservgpu.memory")
+        get_reserv_memnum = ("SELECT COUNT(*)  FROM pcgpu "
+                             "INNER JOIN reservgpu ON pcgpu.gpuid = reservgpu.gpuid "
+                             "WHERE pcgpu.memory <= reservgpu.memory and reservid=%s")
+
+        get_reservnum = ("SELECT COUNT(*)  FROM reservgpu WHERE reservid=%s ")
+
+        del_reservid = ("DELETE  FROM reservgpu WHERE reservid=%s ")
+        del_reservid_reserv = ("DELETE  FROM reserv WHERE reservid=%s ")
+        get_channelid = ("SELECT channelid,pcid FROM reserv where reservid=%s ")
+
+        channelid_list = []
+        self.cursor.execute(get_reserv_single)
+        reserv_single = self.cursor.fetchall()
+        print(reserv_single)
+        for reservid in reserv_single:    
+            self.cursor.execute(get_reserv_memnum,(reservid[0],))
+            reservid_mem_num = self.cursor.fetchall()
+
+            self.cursor.execute(get_reservnum,(reservid[0],))
+            reservid_num = self.cursor.fetchall()
+            if reservid_mem_num == reservid_num :
+                self.cursor.execute(get_channelid,(reservid[0],))
+                channelid_list += self.cursor.fetchall()
+
+                     
+                self.cursor.execute(del_reservid,(reservid[0],))
+                self.cursor.execute(del_reservid_reserv,(reservid[0],))
+        self.cnx.commit()
+        return channelid_list
+    def ls_name(self):
+      self.cursor.execute("select * from pcname")
+      ls_name = self.cursor.fetchall()
+      return ls_name
+                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def __del__(self):
+        self.cursor.close()
+        self.cnx.close()
+    #content['gpu_id']] = { 'size' :content['gpu_size'] , 'mem' : content['gpu_mem'],'name' : content['name'] }
+
+
+
+
+
+
+
+    #   cnx.close()
+
